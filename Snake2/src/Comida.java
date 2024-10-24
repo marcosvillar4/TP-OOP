7fdb1b1d9010b84492782cfe7d9d1f3d20618d30
Nehuel <111355407+Nehuelin@users.noreply.github.com>
2024-10-24 11:05:14 -0300

    Update Comida.java

--- Snake2/src/Comida.java
+++ Snake2/src/Comida.java
@@ -1,19 +1,26 @@
+import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
 public class Comida extends ObjetoJuego {
 
     private final String emoji = "\uD83C\uDF57";
+    private Image imagen;
     private final int ancho;
     private final int cabeza;
 
-    public Comida(Color color, int ancho, int altura) {
+    public Comida(Color color, String imagenPath, int ancho, int altura) {
         super(null, color);
+        loadImage(imagenPath);
         this.ancho = ancho;
         this.cabeza = altura;
         spawn();
     }
 
+    private void loadImage(String imagenPath){
+        ImageIcon icon = new ImageIcon(imagenPath);
+        imagen = icon.getImage();
+    }
     public void spawn() {
         Random rand = new Random();
         int x = rand.nextInt(ancho / Juego.BLOCK_SIZE) * Juego.BLOCK_SIZE;
@@ -23,8 +30,9 @@ public class Comida extends ObjetoJuego {
 
     @Override
     public void render(Graphics g) {
-        g.setFont(new Font("SansSerif", Font.PLAIN, Juego.BLOCK_SIZE));
-        g.drawString(emoji, posicion.x, posicion.y + Juego.BLOCK_SIZE);
+        g.drawImage(imagen, posicion.x, posicion.y, Juego.BLOCK_SIZE, Juego.BLOCK_SIZE, null);
+        // g.setFont(new Font("SansSerif", Font.PLAIN, Juego.BLOCK_SIZE));
+        // g.drawString(emoji, posicion.x, posicion.y + Juego.BLOCK_SIZE);
         // g.setColor(color);
         // g.fillRect(posicion.x, posicion.y, SnakeGame.BLOCK_SIZE, SnakeGame.BLOCK_SIZE);
     }
